68b020cc1725a8e9be657ab21d6e13db46d948ff
Mickey <detrodoniv@yahoo.com>
2019-12-03 09:41:24 -0500

    components

---
+++ monsterclash/src/components/intro.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+class intro extends React.Component(){
+    render(){
+        return(
+            <div>
+                <h1>MONSTER-CLASH</h1>
+                <h3>How to play:</h3>
+                <ol>
+                    <li>create player</li>
+                    <li>create monster</li>
+                    <li>fight!</li>
+                </ol>
+            </div>
+        )
+    }
+}
+export default intro
---
+++ monsterclash/src/components/monster.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+class monster extends React.Component(){
+
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+export default monster
