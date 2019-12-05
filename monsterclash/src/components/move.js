3c043fe8a38520b8c9ec927e67c8f05863997b18
Mickey <detrodoniv@yahoo.com>
2019-12-05 12:57:30 -0500

    fixed fight

--- monsterclash/src/components/move.js
+++ monsterclash/src/components/move.js
@@ -1,33 +1,56 @@
 import React from 'react'
 
 class Move extends React.Component{
-    createmove = ()=> {
+    constructor(){
+        super()
+        this.state= {
+            name: " ",
+            attck: 0,
+            desc: ""
+        }
+    }
+    createmove = (event)=> {
+        event.preventDefault()
+
         fetch('http://localhost:3000/moves',{
             method: "POST",
             header: {
-                'content-type': "application/json",
+                'Content-type': "application/json",
                 accept: "application/json"
             },
             body: JSON.stringify({
-                
+                name: this.state.name,
+                desc: this.state.desc,
+                attack: Math.floor((Math.random() * 50) + 20)
             })
         })
-
-        
     }
+    name = (event)=> {
+        
+        this.setState({
+            name: event.target.value
+        })}
+
+   
+    desc = (event) => {
+        this.setState({
+            desc: event.target.value
+        })
+
+    }
+    
 
     render(){
         return(
             <div>
-               <div>
-                   <form>
+               <div classname="home">
+                   <form onSubmit={this.createmove}>
                        <h2>Create Move</h2>
                        <label>Name</label>
-                       <input type="text" name="name" placeholder="name"/><br/>
-                       <label>Strength</label>
-                       <input type="integer" name="str" value={Math.floor((Math.random() * 50) + 20)}/><br/>
+                        <input type="text" name="name" placeholder="name" onChange={event => this.name(event)}/><br/>
+                       
                        <label>Desciption</label>
-                       <textarea name="desc">
+                        <textarea name="desc" onChange={this.desc}>
                        </textarea><br/>
                        <input type="submit"/>
 
